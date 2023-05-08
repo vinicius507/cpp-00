14d7f8700f309617122a8751ee9998c7dbf14ca4
Vinicius Oliveira <vinicius@myriad.codes>
2023-05-08 10:48:05 -0300

    chore(ex01): adds documentation to commands.hpp and prompt.hpp

--- ex01/src/commands.hpp
+++ ex01/src/commands.hpp
@@ -6,9 +6,14 @@
 /*   By: vgoncalv <vgoncalv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 10:29:32 by vgoncalv          #+#    #+#             */
-/*   Updated: 2023/05/08 10:33:45 by vgoncalv         ###   ########.fr       */
+/*   Updated: 2023/05/08 10:47:40 by vgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/**
+ * @file commands.hpp
+ * @brief Command handlers for the phonebook cli tool.
+ */
 
 #ifndef COMMANDS_HPP
 #define COMMANDS_HPP
@@ -17,8 +22,26 @@
 
 typedef void (*t_cmd_handler)(PhoneBook &book);
 
+/**
+ * @brief Command handler for the ADD command.
+ *
+ * The ADD command adds a Contact to the PhoneBook. It retrieves the Contact
+ * information via standard input.
+ *
+ * @param book The phonebook instance.
+ */
 void cmd_add(PhoneBook &book);
 
+/**
+ * @brief Command handler for the SEARCH command.
+ *
+ * The SEARCH command prints a table with all contacts fields "Id", "First
+ * Name", "Last Name" and "Nickname" and prompts the user to select a contact.
+ *
+ * After selecting a contact, the contact information is displayed to the user.
+ *
+ * @param book The phonebook instance.
+ */
 void cmd_search(PhoneBook &book);
 
 #endif // !COMMANDS_HPP
--- ex01/src/prompt.hpp
+++ ex01/src/prompt.hpp
@@ -6,15 +6,27 @@
 /*   By: vgoncalv <vgoncalv@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 10:32:21 by vgoncalv          #+#    #+#             */
-/*   Updated: 2023/05/08 10:32:51 by vgoncalv         ###   ########.fr       */
+/*   Updated: 2023/05/08 10:44:20 by vgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/**
+ * @file prompt.hpp
+ * @brief Prompt helper function header file.
+ */
 
 #ifndef PROMPT_HPP
 #define PROMPT_HPP
 
 #include <string>
 
+/**
+ * @brief Shows a prompt with a `prefix` and captures a line from standard
+ * input.
+ *
+ * @param prefix The prompt prefix.
+ * @return The input of the user.
+ */
 std::string prompt(std::string prefix);
 
 #endif // !PROMPT_HPP
