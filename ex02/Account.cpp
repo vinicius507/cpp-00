79595259f899148c3f6b726ff93a7fc404215d23
Vinicius Oliveira <vinicius@myriad.codes>
2023-05-09 07:51:42 -0300

    feat(ex02): implements missing methods

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -85,6 +85,15 @@ bool Account::makeWithdrawal(int withdrawal) {
   return (true);
 }
 
+int Account::checkAmount(void) const { return (this->_amount); }
+
+void Account::displayStatus() const {
+  Account::_displayTimestamp();
+  std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount
+            << ";deposits:" << this->_nbDeposits
+            << ";withdrawals:" << this->_nbWithdrawals << std::endl;
+}
+
 void Account::_displayTimestamp(void) {
   char buf[19] = {'\0'};
   time_t now = time(NULL);
