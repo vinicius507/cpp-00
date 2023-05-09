245d5da65b37ff53dd57c503f9577dd3fb880e3f
Vinicius Oliveira <vinicius@myriad.codes>
2023-05-09 07:30:27 -0300

    feat(ex02): implements account creation

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -11,8 +11,28 @@
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <iostream>
+#include <time.h>
 
 int Account::_nbAccounts = 0;
 int Account::_totalAmount = 0;
 int Account::_totalNbDeposits = 0;
 int Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit) {
+  this->_amount = initial_deposit;
+  this->_accountIndex = Account::_nbAccounts++;
+  Account::_totalAmount += initial_deposit;
+
+  Account::_displayTimestamp();
+  std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount
+            << ";created" << std::endl;
+}
+
+void Account::_displayTimestamp(void) {
+  char buf[19] = {'\0'};
+  time_t now = time(NULL);
+
+  strftime(buf, sizeof(buf), "[%Y%m%d_%H%M%S] ", localtime(&now));
+  std::cout << buf;
+}
