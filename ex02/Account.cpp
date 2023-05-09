e10396dc6e221c363b87840fb8f09eb18a91450f
Vinicius Oliveira <vinicius@myriad.codes>
2023-05-09 07:27:13 -0300

    feat(ex02): initialise Account static attributes

---
+++ ex02/Account.cpp
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgoncalv <vgoncalv@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 21:04:50 by vgoncalv          #+#    #+#             */
+/*   Updated: 2023/05/08 21:37:22 by vgoncalv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
